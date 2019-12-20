951f484df4f94fc94db03702a263f9310b8dbc2c
Mohamed Sedki <mohamedsedkipro@gmail.com>
2019-12-20 16:30:30 +0100

    nav to ex3

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:sanabelsecondexercice/components/models/LetterBalloon.dart';
 import 'package:sanabelsecondexercice/components/widgets/Balloon.dart';
 import 'package:sanabelsecondexercice/components/widgets/PerrineAppBar.dart';
+import 'package:sanabelsecondexercice/pages/Exercice3.dart';
 import 'package:sanabelsecondexercice/theme/style.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,33 +14,53 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<LettreBalloon> letters = [
     new LettreBalloon(letter: 'أ', letterColor: 'red', balloonColor: 'pink'),
-    new LettreBalloon(letter: 'ب', letterColor: 'yellow', balloonColor: 'green'),
-    new LettreBalloon(letter: 'ت', letterColor: 'white', balloonColor: 'orange'),
+    new LettreBalloon(
+        letter: 'ب', letterColor: 'yellow', balloonColor: 'green'),
+    new LettreBalloon(
+        letter: 'ت', letterColor: 'white', balloonColor: 'orange'),
     new LettreBalloon(letter: 'ث', letterColor: 'red', balloonColor: 'blue'),
-    new LettreBalloon(letter: 'ج', letterColor: 'orange', balloonColor: 'blueaccent'),
-    new LettreBalloon(letter: 'ح', letterColor: 'orange', balloonColor: 'yellow'),
-    new LettreBalloon(letter: 'خ', letterColor: 'yellow', balloonColor: 'purple'),
+    new LettreBalloon(
+        letter: 'ج', letterColor: 'orange', balloonColor: 'blueaccent'),
+    new LettreBalloon(
+        letter: 'ح', letterColor: 'orange', balloonColor: 'yellow'),
+    new LettreBalloon(
+        letter: 'خ', letterColor: 'yellow', balloonColor: 'purple'),
     new LettreBalloon(letter: 'د', letterColor: 'red', balloonColor: 'pink'),
-    new LettreBalloon(letter: 'ذ', letterColor: 'yellow', balloonColor: 'green'),
-    new LettreBalloon(letter: 'ر', letterColor: 'white', balloonColor: 'orange'),
+    new LettreBalloon(
+        letter: 'ذ', letterColor: 'yellow', balloonColor: 'green'),
+    new LettreBalloon(
+        letter: 'ر', letterColor: 'white', balloonColor: 'orange'),
     new LettreBalloon(letter: 'ز', letterColor: 'red', balloonColor: 'blue'),
-    new LettreBalloon(letter: 'س', letterColor: 'orange', balloonColor: 'blueaccent'),
-    new LettreBalloon(letter: 'ش', letterColor: 'orange', balloonColor: 'yellow'),
-    new LettreBalloon(letter: 'ص', letterColor: 'yellow', balloonColor: 'purple'),
+    new LettreBalloon(
+        letter: 'س', letterColor: 'orange', balloonColor: 'blueaccent'),
+    new LettreBalloon(
+        letter: 'ش', letterColor: 'orange', balloonColor: 'yellow'),
+    new LettreBalloon(
+        letter: 'ص', letterColor: 'yellow', balloonColor: 'purple'),
     new LettreBalloon(letter: 'ض', letterColor: 'red', balloonColor: 'pink'),
-    new LettreBalloon(letter: 'ط', letterColor: 'yellow', balloonColor: 'green'),
-    new LettreBalloon(letter: 'ظ', letterColor: 'white', balloonColor: 'orange'),
+    new LettreBalloon(
+        letter: 'ط', letterColor: 'yellow', balloonColor: 'green'),
+    new LettreBalloon(
+        letter: 'ظ', letterColor: 'white', balloonColor: 'orange'),
     new LettreBalloon(letter: 'ع', letterColor: 'red', balloonColor: 'blue'),
-    new LettreBalloon(letter: 'غ', letterColor: 'orange', balloonColor: 'blueaccent'),
-    new LettreBalloon(letter: 'ف', letterColor: 'orange', balloonColor: 'yellow'),
-    new LettreBalloon(letter: 'ق', letterColor: 'yellow', balloonColor: 'purple'),
+    new LettreBalloon(
+        letter: 'غ', letterColor: 'orange', balloonColor: 'blueaccent'),
+    new LettreBalloon(
+        letter: 'ف', letterColor: 'orange', balloonColor: 'yellow'),
+    new LettreBalloon(
+        letter: 'ق', letterColor: 'yellow', balloonColor: 'purple'),
     new LettreBalloon(letter: 'ك', letterColor: 'red', balloonColor: 'pink'),
-    new LettreBalloon(letter: 'ل', letterColor: 'yellow', balloonColor: 'green'),
-    new LettreBalloon(letter: 'م', letterColor: 'white', balloonColor: 'orange'),
+    new LettreBalloon(
+        letter: 'ل', letterColor: 'yellow', balloonColor: 'green'),
+    new LettreBalloon(
+        letter: 'م', letterColor: 'white', balloonColor: 'orange'),
     new LettreBalloon(letter: 'ن', letterColor: 'red', balloonColor: 'blue'),
-    new LettreBalloon(letter: 'ه', letterColor: 'red', balloonColor: 'blueaccent'),
-    new LettreBalloon(letter: 'و', letterColor: 'orange', balloonColor: 'yellow'),
-    new LettreBalloon(letter: 'ي', letterColor: 'orange', balloonColor: 'purple'),
+    new LettreBalloon(
+        letter: 'ه', letterColor: 'red', balloonColor: 'blueaccent'),
+    new LettreBalloon(
+        letter: 'و', letterColor: 'orange', balloonColor: 'yellow'),
+    new LettreBalloon(
+        letter: 'ي', letterColor: 'orange', balloonColor: 'purple'),
   ];
 
   @override
@@ -64,19 +85,31 @@ class _HomePageState extends State<HomePage> {
               var a = i + 1;
               if ((a % 5 == 0) && (a != 0)) {
                 print(a);
-                return Container(
-                  width: screenSize.width / 10,
-                  height: screenSize.width / 10,
-                  child: Image.asset(
-                    'assets/recap.png',
-                    fit: BoxFit.contain,
+                return InkWell(
+                 
+                  child: Container(
+                    width: screenSize.width / 10,
+                    height: screenSize.width / 10,
+                    child: Image.asset(
+                      'assets/recap.png',
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 );
               } else {
-                return Balloon(
-                    letter: letters[a - 1].letter,
-                    letterColor: letters[a - 1].letterColor,
-                    balloonColor: letters[a - 1].balloonColor);
+                return InkWell(
+                   onTap: () {
+                    print('to ex 3');
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (BuildContext context) {
+                      return ExerciceThree();
+                    }));
+                  },
+                                  child: Balloon(
+                      letter: letters[a - 1].letter,
+                      letterColor: letters[a - 1].letterColor,
+                      balloonColor: letters[a - 1].balloonColor),
+                );
               }
             },
           ),
