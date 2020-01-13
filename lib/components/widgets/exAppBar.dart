ae54f97399b6ebf8f3c1e95d76882a36945c09f4
Mohamed Sedki <mohamedsedkipro@gmail.com>
2020-01-13 10:19:02 +0100

    making appbar responsive

--- lib/components/widgets/exAppBar.dart
+++ lib/components/widgets/exAppBar.dart
@@ -5,13 +5,13 @@ import '../ExQuestionBar.dart';
 
 Widget exAppBar(screenSize, _scaffoldKey, ExQuestionBar exQuestionBar) {
   return PreferredSize(
-    preferredSize: Size.fromHeight(screenSize.height / 10),
+    preferredSize: Size.fromHeight(screenSize.height / 8),
     child: AppBar( 
       backgroundColor: Colors.transparent,
       elevation: 0.0,
       automaticallyImplyLeading: false,
       leading: Container(
-         alignment: Alignment.topCenter,
+         alignment: Alignment.center,
         child: InkWell(
           child: Icon(
             Icons.menu,
